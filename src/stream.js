6261820b9a9dbdea1793e444e09796410cfe06af
Harsh Vakharia <harshjv@gmail.com>
2016-11-05 14:36:32 +0530

    * Fix node_module/.bin path resolution * Spawn only one Quark process for multi-process app * Add electron's IPC communication

--- src/stream.js
+++ src/stream.js
@@ -3,9 +3,11 @@ import xtend from 'xtend'
 import { spawn } from 'child_process'
 import { parse } from 'shell-quote'
 
-const PATH_SEP = process.platform === 'win32' ? ';' : ':'
-const PATH_KEY = process.platform === 'win32' && !(process.env.PATH && !process.env.Path) ? 'Path' : 'PATH'
-const ESCAPE_CHAR = process.platform === 'win32' ? '^' : '\\'
+const { platform, env } = process
+
+const PATH_SEP = platform === 'win32' ? ';' : ':'
+const PATH_KEY = platform === 'win32' && !(env.PATH && !env.Path) ? 'Path' : 'PATH'
+const ESCAPE_CHAR = platform === 'win32' ? '^' : '\\'
 const ELECTRON_LIB = `electron ${path.join(__dirname, 'electron.bundle.js')}`
 
 const commandToString = (cmd) => cmd.pattern || cmd.op || handleQuotes(cmd)
@@ -16,25 +18,15 @@ const handleQuotes = (s) => {
   return String(s).replace(/([\\$`()!#&*|])/g, '\\$1')
 }
 
-const NPMBinPath = (cwd, PATH) => {
-  const result = []
-  let parent
-
-  while (true) {
-    parent = path.join(cwd, '..')
-    result.push(path.join(cwd, 'node_modules/.bin'))
-    if (parent === cwd) return result.concat(PATH).join(PATH_SEP)
-    cwd = parent
-  }
-}
+const pathToNPMBinDirectory = () => path.resolve(__dirname, '..', 'node_modules/.bin')
 
 const formatAndSpawn = (command, opts) => {
   const options = xtend({}, opts)
   let file
   let args
 
-  if (process.platform === 'win32') {
-    file = process.env.comspec || 'cmd.exe'
+  if (platform === 'win32') {
+    file = env.comspec || 'cmd.exe'
     args = [ '/s', '/c', '"' + command + '"' ]
     options.windowsVerbatimArguments = true
   } else {
@@ -51,17 +43,16 @@ const formatAndSpawn = (command, opts) => {
   return spawn(file, args, options)
 }
 
-module.exports = () => {
-  const cwd = process.cwd()
-  const env = process.env
+const handleSpawningInMainProcess = () => {
+  let out
   const parsed = parse(ELECTRON_LIB, xtend({ '': '$' }), { escape: ESCAPE_CHAR }).map(commandToString).join(' ')
 
-  const override = {
-    [ PATH_KEY ]: NPMBinPath(path.resolve(cwd, '.'), env[PATH_KEY] || process.env[PATH_KEY])
+  const overridePATH = {
+    [ PATH_KEY ]: [ pathToNPMBinDirectory(), env[PATH_KEY] ].join(PATH_SEP)
   }
 
-  const child = formatAndSpawn(parsed, { env: xtend(env, override) })
-  let out = child.stdin.write.bind(child.stdin)
+  const child = formatAndSpawn(parsed, { env: xtend(env, overridePATH) })
+  out = child.stdin.write.bind(child.stdin)
 
   child.stdin.setEncoding('utf-8')
   child.stdout.pipe(process.stdout)
@@ -71,4 +62,36 @@ module.exports = () => {
   })
 
   return (log) => out(log)
+}
+
+module.exports = () => {
+  if (typeof process === 'undefined') {
+    // Web browser
+
+    return console.log.bind(console)
+  } else {
+    if (process.type === 'renderer') {
+      // Renderer process
+      const { ipcRenderer } = require('electron')
+
+      return (log) => ipcRenderer.send('quark-logger:ipc', log)
+    } else {
+      if (process.versions.electron) {
+        // Electron
+        const { ipcMain } = require('electron')
+
+        let logger = handleSpawningInMainProcess()
+
+        ipcMain.on('quark-logger:ipc', (event, arg) => {
+          logger(arg)
+        })
+
+        return logger
+      } else {
+        // Node.js
+
+        return handleSpawningInMainProcess()
+      }
+    }
+  }
 }
